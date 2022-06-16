6d6162e2ace156f7bd84121a54df5912fed00117
NhatHa Nguyen <61714270+ngnhatha@users.noreply.github.com>
2022-06-16 07:14:59 +0000

    JSON

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementById('demo').innerHTML = 'Hello JavaScript';
+let text = '{"employees":[' +
+'{"firstName":"John","lastName":"Doe" },' +
+'{"firstName":"Anna","lastName":"Smith" },' +
+'{"firstName":"Peter","lastName":"Jones" }]}';
+
+const obj = JSON.parse(text);
+document.getElementById("demo").innerHTML =
+obj.employees[1].firstName + " " + obj.employees[1].lastName;
